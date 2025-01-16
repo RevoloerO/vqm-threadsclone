f9f6a4d7498fe7011296272d36b8dd825578df61
Vuong Quyen Mai <59490873+RevoloerO@users.noreply.github.com>
2025-01-15 21:57:40 -0500

    v0.1.4b
    - bug fixes
    - add notification for metamask connection

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import headerImg1 from './assets/threads-clone-logo.jpeg';
 
 function App() {
   const [account, setAccount] = useState(null);
-  const [threads, setThreads] = useState([][]);
+  const [threads, setThreads] = useState([]);
 
   const ShowThreads = () => {
     const web3 = new Web3(Web3.givenProvider);
@@ -39,9 +39,11 @@ function App() {
           const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
           setAccount(accounts[0]);
         } catch (error) {
+          getelementbyId('message').innerHTML = 'Error connecting to MetaMask';
           console.error('Error connecting to MetaMask:', error);
         }
       } else {
+        getelementbyId('message').innerHTML = 'MetaMask not detected';
         console.error('MetaMask not detected');
       }
     };
@@ -55,7 +57,7 @@ function App() {
               <img id="header-img-1" src={headerImg1} alt=""></img>
               <h1>THREADS</h1>
               <h2>Decentralized Social Media</h2>
-              <p>Connect your wallet to get started</p>
+              <p id='message'>Connect your wallet to get started</p>
               {account ? (
                 <p>Connected account: {account}</p>
               ) : (
